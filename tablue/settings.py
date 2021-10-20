0c4d1ad49c672f5fbf7d1ce4c3ab9c270063e843
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-20 12:38:58 +0300

    add installed apps and middleware to settings lists

--- tablue/settings.py
+++ tablue/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'design.apps.DesignConfig',
+    'django.contrib.postgres',
+    'cloudinary_storage',
+    'cloudinary',
+    'registration',    
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
 ]
 
 ROOT_URLCONF = 'tablue.urls'
