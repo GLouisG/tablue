3ac3a5e4b9b33d3df7c07df0cb593edbb7d07390
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-20 12:36:50 +0300

    connect to database

--- tablue/settings.py
+++ tablue/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'tablue.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tablue',
+        'USER': 'mikeywalked',
+        'PASSWORD':'practice',        
+    }      
 }
 
 
