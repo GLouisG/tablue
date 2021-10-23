cf0fac1f60737514360fbe9a6e688beee5a986fd
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 13:02:12 +0300

    register Project class in admins

--- design/admin.py
+++ design/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from design.models import Profile, Project, Rating
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Rating)
