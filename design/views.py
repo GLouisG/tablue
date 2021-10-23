8058828a22fad5c24a799fdb2748eef81be393d2
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 15:03:45 +0300

    set up home view function

--- design/views.py
+++ design/views.py
@@ -6,3 +6,7 @@ from design.models import Project, Rating
 from design.forms import NewProjForm, RateForm
 from django.db.models.base import ObjectDoesNotExist
 # Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    return render(request, "index.html", {"projs":projects,})
+    
