1569b27c6bddb00763b21be5cdeac7db09a9f687
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 15:20:49 +0300

    add RateForm to forms.py

--- design/forms.py
+++ design/forms.py
@@ -6,3 +6,8 @@ class NewProjForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['owner']
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['owner', 'project']
