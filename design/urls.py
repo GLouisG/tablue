887768bafe2b5d41d62067ac85de8647ddea875c
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 16:00:27 +0300

    add project url to design urls

--- design/urls.py
+++ design/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^search/',  views.search, name='search'),
     url(r'^update/profile$', views.update_profile, name='update_profile'),
+    url(r'^project/(\d+)', views.single, name='single'),
 
 
 ]
