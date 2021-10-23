f39df00d8b1d96f3c6ec98f3ea293d13ce24aafc
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 16:18:16 +0300

    add accounts url to tablue urls.py

--- tablue/urls.py
+++ tablue/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('design.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),    
+   
 ]
