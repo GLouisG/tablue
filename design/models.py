bcb872c4509598386b2e4aeaab015a6eae874235
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 12:51:29 +0300

    add profile class

--- design/models.py
+++ design/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 # Create your models here.
+
+           
+class Profile(models.Model):
+      user = models.OneToOneField(User, on_delete=models.CASCADE)    
+      bio = models.TextField(max_length=500, default=f'This is my bio, Welcome!')
+      contacts = models.TextField(max_length=500, default=f'Contacts:')
+      
