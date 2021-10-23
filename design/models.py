10c0be3af3c8f3b80a591cb79430e5c82699fe3b
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 12:56:04 +0300

    add rating class

--- design/models.py
+++ design/models.py
@@ -35,3 +35,12 @@ class Project(models.Model):
             self.delete() 
       def __str__(self):
             return f'Project {self.title}'               
+
+class Rating(models.Model):
+      design = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10)])
+      usability = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10)])                  
+      content=models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10)]) 
+      owner = models.ForeignKey('Profile', on_delete=models.CASCADE)
+      project = models.ForeignKey('Project', on_delete=models.CASCADE)
+      def __str__(self):
+            return f'For project {self.project.title}'              
