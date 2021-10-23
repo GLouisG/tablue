01fe189917af49f260e11c2c7f5d4fd88e6e055d
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-23 15:26:06 +0300

    add Profile Update Form

--- design/forms.py
+++ design/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from design.models import Project
+from design.models import Profile, Project, Rating
+
 
 class NewProjForm(forms.ModelForm):
     class Meta:
@@ -9,5 +10,11 @@ class NewProjForm(forms.ModelForm):
 
 class RateForm(forms.ModelForm):
     class Meta:
-        model = Project
+        model = Rating
         exclude = ['owner', 'project']
+
+class ProfUpdateForm(forms.ModelForm):
+    class Meta:
+          model = Profile
+          exclude = ['user']          
+#          fields = ('pic', 'bio', 'contacts')  
