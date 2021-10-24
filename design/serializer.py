7c62f086023ed06d66d70216a4fe44e9005f93f6
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-24 20:38:18 +0300

    add profile serializer

--- design/serializer.py
+++ design/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Profile, Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('id','title','pic', 'description', 'link', 'owner') 
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user','bio','pic', 'contacts',) 
